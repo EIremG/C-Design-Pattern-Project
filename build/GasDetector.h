39cb384e26601afaa3843c7f4c7d7bca0196c426
EIremG <enimutries@gmail.com>
2025-12-11 23:20:57 +0300

    Reset irem branch and add new Device Framework

--- build/GasDetector.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Detector.h"
-
-class GasDetector : public Detector {
-public:
-    GasDetector(const std::string& name = "Gas Detector");
-    void detect() override;
-};
--- build/SmokeDetector.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-#include "Detector.h"
-
-class SmokeDetector : public Detector {
-public:
-    SmokeDetector(const std::string& name = "Smoke Detector");
-    void detect() override;
-};
