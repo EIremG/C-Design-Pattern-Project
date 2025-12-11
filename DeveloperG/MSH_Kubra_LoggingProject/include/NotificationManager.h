e6330c7a9048b777066678485858be1f4ceda875
kubrakpinar28 <kubra.akpinar028@gmail.com>
2025-12-12 01:13:35 +0300

    NotificationManager arayüzü eklendi (preference ve failure notification)

---
+++ DeveloperG/MSH_Kubra_LoggingProject/include/NotificationManager.h
@@ -0,0 +1,21 @@
+#ifndef NOTIFICATION_MANAGER_H
+#define NOTIFICATION_MANAGER_H
+
+#include <string>
+#include "Types.h"
+
+// LLR19 & LLR36: Kullanıcı bildirim tercihine göre arıza bildirimi
+class NotificationManager {
+public:
+    // Bildirim tercihinin ayarlanması (LLR36)
+    void setPreference(NotificationType type);
+    NotificationType getPreference() const;
+
+    // LLR19: Cihaz arızasını kullanıcıya bildir
+    void notifyFailure(int deviceId, const std::string& reason) const;
+
+private:
+    NotificationType m_type{NotificationType::CONSOLE};
+};
+
+#endif // NOTIFICATION_MANAGER_H
