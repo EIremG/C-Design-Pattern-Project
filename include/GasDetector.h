1b433727598ab762a0e586d82fe03c8c85e0d8a7
İrem GÖNÜLAL <137067262+EIremG@users.noreply.github.com>
2025-12-14 16:38:02 +0300

    Add detect method and remove checkCondition method

--- include/GasDetector.h
+++ include/GasDetector.h
@@ -5,15 +5,15 @@ class GasDetector : public Detector {
 private:
     float gasConcentration;
 
-protected:
-    bool checkCondition() const override;
-
 public:
     GasDetector(const std::string& name, float sensitivity = 0.7f, float threshold = 100.0f);
 
     bool powerOn() override;
     std::string getStatus() const override;
     Device* clone() const override;
+
+    // Detector's pure virtual function
+    bool detect() override;
 
     void setGasConcentration(float concentration) { gasConcentration = concentration; }
     float getGasConcentration() const { return gasConcentration; }
