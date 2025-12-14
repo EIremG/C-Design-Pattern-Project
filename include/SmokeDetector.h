baf1f936050b9a5c5c317f1a858a8a1bbbc7e952
İrem GÖNÜLAL <137067262+EIremG@users.noreply.github.com>
2025-12-14 16:35:17 +0300

    Add detect method to SmokeDetector class

--- include/SmokeDetector.h
+++ include/SmokeDetector.h
@@ -5,15 +5,15 @@ class SmokeDetector : public Detector {
 private:
     float particleDensity;
 
-protected:
-    bool checkCondition() const override;
-
 public:
     SmokeDetector(const std::string& name, float sensitivity = 0.7f, float threshold = 50.0f);
 
     bool powerOn() override;
     std::string getStatus() const override;
     Device* clone() const override;
+
+    // Detector's pure virtual function
+    bool detect() override;
 
     void setParticleDensity(float density) { particleDensity = density; }
     float getParticleDensity() const { return particleDensity; }
